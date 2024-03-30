8ac7df485141da360023c83901f79e07e70f20bd
nana2503 <87693106+nana2503@users.noreply.github.com>
2024-03-30 19:59:01 +0700

    test homepage with wrap widget

--- lib/screens/adminHome_page.dart
+++ lib/screens/adminHome_page.dart
@@ -12,30 +12,38 @@ class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.5,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                ),
-                child: Text("Chức năng 1"),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                width: MediaQuery.of(context).size.width * 0.5,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                ),
-                child: Text("Chức năng 2"),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+        body: Wrap(
+      spacing: 8.0,
+      runSpacing: 4.0,
+      children: <Widget>[
+        Chip(
+          label: const Text("Ngọc Na"),
+          avatar: CircleAvatar(
+            backgroundColor: Colors.blue.shade900,
+            child: const Text("NN"),
+          ),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.blue.shade900, child: const Text('AH')),
+          label: const Text('Hamilton'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.blue.shade900, child: const Text('ML')),
+          label: const Text('Lafayette'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.blue.shade900, child: const Text('HM')),
+          label: const Text('Mulligan'),
+        ),
+        Chip(
+          avatar: CircleAvatar(
+              backgroundColor: Colors.blue.shade900, child: const Text('JL')),
+          label: const Text('Laurens'),
+        ),
+      ],
+    ));
   }
 }
