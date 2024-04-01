491b1dcf8d2fe5c13cee7a40f490fc2e1b8b6d5e
nana2503 <87693106+nana2503@users.noreply.github.com>
2024-04-01 19:07:12 +0700

    update count student

--- server/src/controller/classController.js
+++ server/src/controller/classController.js
@@ -1,95 +1,95 @@
 //src/controller/lasController.js
-import classService from '../service/classService';
-const readFunc = async(req, res)=>{
-try {
-    let data=await classService.getClass();
+import classService from "../service/classService";
+const readFunc = async (req, res) => {
+  try {
+    let data = await classService.getClass();
 
     return res.status(200).json({
-        EM : data.EM,
-        EC: data.EC,
-        DT: data.DT
-
-    })
-    
-} catch (error) {
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
+  } catch (error) {
     console.log(e);
     return res.status(500).json({
-        EM : 'error from server',
-        EC: '-1',
-        DT:''
-})
-}
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    });
+  }
 };
-const createFunc =async(req,res)=>{
- 
-    try {
-        let data=await classService.createNewClass(req.body);
-        return res.status(200).json({
-            EM : data.EM,
-            EC: data.EC,
-            DT: data.DT
-    
-        })
-    } catch (error) {
-        return res.status(500).json({
-            EM : 'error from server',
-            EC: '-1',
-            DT:''
+const createFunc = async (req, res) => {
+  try {
+    let data = await classService.createNewClass(req.body);
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    });
+  }
+};
+const updateFunc = async (req, res) => {
+  try {
+    let data = await classService.updateClass(req.body);
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    });
+  }
+};
+const deleteFunc = async (req, res) => {
+  try {
+    let data = await classService.deleteClass(req.body.userId);
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    });
+  }
+};
+const getClassListAndStudent = async (req, res) => {
+  try {
+    let data = await classService.countStudentInClass();
+    let newDataWithCount = data.DT.map(classItem => {
+        let updatedClassItem = {classItem, "count": classItem.Users.length};
+        return updatedClassItem;
     })
-    }
-}
-const updateFunc =async(req,res)=>{
-    try {
-        let data=await classService.updateClass(req.body);
-        return res.status(200).json({
-            EM : data.EM,
-            EC: data.EC,
-            DT: data.DT
-    
-        })
-    } catch (error) {
-        return res.status(500).json({
-            EM : 'error from server',
-            EC: '-1',
-            DT:''
-    })
-    }
-}
-const deleteFunc =async(req,res)=>{
-  
-    try {
-        let data =await classService.deleteClass(req.body.userId);
-        return res.status(200).json({
-            EM : data.EM,
-            EC: data.EC,
-            DT: data.DT
-    
-        })
-    } catch (error) {
-            return res.status(500).json({
-                EM : 'error from server',
-                EC: '-1',
-                DT:''
-    })
-    }
-}
-const getClassListAndStudent = async(req, res) => {
-    try {
-        let data = await classService.countStudentInClass();
-        return res.status(200).json({
-            EM : data.EM,
-            EC: data.EC,
-            DT: data.DT
-    
-        })
-    }catch(e) {
-        return res.status(500).json({
-            EM : 'error from server',
-            EC: '-1',
-            DT:''
-})
-    }
-}
-module.exports={
-    readFunc,createFunc,updateFunc,deleteFunc,getClassListAndStudent
-}
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: newDataWithCount,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    });
+  }
+};
+module.exports = {
+  readFunc,
+  createFunc,
+  updateFunc,
+  deleteFunc,
+  getClassListAndStudent,
+};
